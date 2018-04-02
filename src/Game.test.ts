5d7519d1e88cee9f7a3da8af0654ac9ca18d7453
James Howard <jameshoward@mac.com>
2018-04-02 16:53:01 -0700

    Add some tests for win condition checking

--- src/Game.test.ts
+++ src/Game.test.ts
@@ -1,5 +1,7 @@
 import * as Game from './Game'
 
+// *** Adjacency Count Tests ***
+
 it('calculates adjacency 3x3 upper left', () => {
     let b = Game.Board.fromPattern([
         "*  ",
@@ -42,3 +44,36 @@ it('calculates 3x3 adjacency 8 mines', () => {
     ]);
     expect(b.adjacentMineCount(1, 1)).toBe(8);
 });
+
+// *** GameState tests ***
+
+it('wins 3x3 game via flag', () => {
+    let b = Game.Board.fromPattern([
+        "   ",
+        " * ",
+        "   "
+    ]);
+    expect(b.state).toBe(Game.GameState.IN_PROGRESS);
+    let c = b.flag(1, 1);
+    expect((c & Game.Cell.FLAGGED) != 0).toBe(true);
+    expect(b.state).toBe(Game.GameState.WON);
+});
+
+it('wins 3x3 game via reveal', () => {
+    let b = Game.Board.fromPattern([
+        "   ",
+        " * ",
+        "   "
+    ]);
+    expect(b.state).toBe(Game.GameState.IN_PROGRESS);
+    for (var y = 0; y < 3; y++) {
+        for (var x = 0; x < 3; x++) {
+            if (x === 1 && y === 1) continue;
+            expect(b.state).toBe(Game.GameState.IN_PROGRESS);
+            let c = b.reveal(x, y);
+            expect((c & Game.Cell.REVEALED) != 0).toBe(true);
+        }
+    }
+    expect(b.state).toBe(Game.GameState.WON);
+});
+
