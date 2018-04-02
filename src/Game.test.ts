6f492a8d485ed7652c3ffa39256592be2df08396
James Howard <jameshoward@mac.com>
2018-04-02 16:22:02 -0700

    Add some initial tests for the Game model. Currently not passing.

---
+++ src/Game.test.ts
@@ -0,0 +1,35 @@
+import * as Game from './Game'
+
+it('calculates adjacency 3x3 upper left', () => {
+    let b = Game.Board.fromPattern([
+        "*  ",
+        "   ",
+        "   "
+    ]);
+    expect(b.cellAt(0, 0)).toBe(Game.Cell.MINED);
+    expect(b.adjacentMineCount(1, 0)).toBe(1);
+    expect(b.adjacentMineCount(2, 0)).toBe(0);
+    expect(b.adjacentMineCount(0, 1)).toBe(1);
+    expect(b.adjacentMineCount(1, 1)).toBe(1);
+    expect(b.adjacentMineCount(2, 1)).toBe(0);
+    expect(b.adjacentMineCount(0, 2)).toBe(0);
+    expect(b.adjacentMineCount(1, 2)).toBe(0);
+    expect(b.adjacentMineCount(2, 2)).toBe(0);
+});
+
+it('calculates adjacency 3x3 center', () => {
+    let b = Game.Board.fromPattern([
+        "   ",
+        " * ",
+        "   "
+    ]);
+    expect(b.cellAt(1, 1)).toBe(Game.Cell.MINED);
+    for (var y = 0; y <= 2; y++) {
+        for (var x = 0; x <= 2; x++) {
+            if (x === 1 && y === 1) {
+                continue;
+            }
+            expect(b.adjacentMineCount(x, y)).toBe(1);
+        }
+    }
+})
